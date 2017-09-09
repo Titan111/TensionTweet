c36d8a4487fdb9f0a20bc999c3d3b32e09a12990
Titan111 <tomo_develop_singularity_111@ezweb.ne.jp>
2017-09-09 18:54:41 +0900

    complete change image when firstlater a tweet

--- main.py
+++ main.py
@@ -3,16 +3,19 @@
 import tweepy 
 import twipy 
 
+USER_NAME = "titan_FF"
 if __name__ == "__main__":
-	api = twipy.make("tomo_iwa.json")
-	api.update_profile_image(filename="./test.jpg")
-	api.update_profile_banner(filename="./test.jpg")
-#	try:
-#		api.update_profile_background_image(filename="./test.jpg", use=1, tile=0)
-#	except tweepy.error.TweepError as err:
-#		if err[0][0]['code'] == 131:
-#			print( "resulted in "+err+".")
-#			sleep(1)
-#		else:
-#			raise
 
+	api = twipy.make("tomo_iwa.json")
+
+	timelines = api.home_timeline()
+	my_status = list(filter(lambda x: x.user.screen_name == USER_NAME,timelines))
+
+	first_char = my_status[0].text[0]
+	if first_char == 'a':
+		api.update_profile_banner(filename="./test.jpg")
+	else:
+		api.update_profile_banner(filename="./std_banner.jpg")
+
+#	api.update_profile_image(filename="./.jpg")
+#	api.update_profile_banner(filename="./std_banner.jpg")
