fe13415e152148f2845b1fe1897d46dafa84c8fb
Titan111 <tomo_develop_singularity_111@ezweb.ne.jp>
2017-09-09 15:45:24 +0900

    complete twipy

--- twipy.py
+++ twipy.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
-
+import json
 import tweepy
+
+def make(filename):
+	f = open('tomo_iwa.json')
+	key_dict = json.load(f)["ConsumerKey"]
+	auth = tweepy.OAuthandler(key_dict["ConsumerKey"],key_dict["ConsumerSecret"])
+	auth.set_access_token(key_dict["AccessToken"],key_dict["AccessTokenSecret"])
+	return auth
+
+
+
