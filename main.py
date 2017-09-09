3938fd3f3d5200807651b2fbe2c3f8c46b8099a0
Titan111 <tomo_develop_singularity_111@ezweb.ne.jp>
2017-09-09 20:41:18 +0900

    complete change image when firstlater a tweet

--- main.py
+++ main.py
@@ -2,6 +2,17 @@
 
 import tweepy 
 import twipy 
+import urllib
+
+API_KEY = "1569AB145D7B6E3A8F1D049372D96ED81B653130"
+def feel_api(text):
+	url = "http://ap.mextractr.net/ma9/emotion_analyzer?out=json"
+	params_txt = urllib.parse.urlencode(
+		{'apikey':API_KEY,
+		 'text':text})
+	print(url+params_txt)
+	response = urllib.request.urlopen(url+"&"+params_txt)
+	return response.read()
 
 USER_NAME = "titan_FF"
 if __name__ == "__main__":
@@ -10,12 +21,15 @@ if __name__ == "__main__":
 
 	timelines = api.home_timeline()
 	my_status = list(filter(lambda x: x.user.screen_name == USER_NAME,timelines))
+	status_feel = feel_api(my_status[0].text)
+	feel_dict = json.loads(status_feel.decode('utf-8'))
+	print(feel_dict)
 
-	first_char = my_status[0].text[0]
-	if first_char == 'a':
-		api.update_profile_banner(filename="./test.jpg")
-	else:
-		api.update_profile_banner(filename="./std_banner.jpg")
+#	
+#	if first_char == 'a':
+#		api.update_profile_banner(filename="./test.jpg")
+#	else:
+#		api.update_profile_banner(filename="./std_banner.jpg")
 
 #	api.update_profile_image(filename="./.jpg")
 #	api.update_profile_banner(filename="./std_banner.jpg")
